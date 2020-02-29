9773f9315d7d78c4de0e81925d13326a0a963f70
shadow_walker <shadowwalkersb@gmail.com>
2020-02-28 23:04:35 -0600

    LIBRARY_INC

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
 setx YES_I_HAVE_THE_RIGHT_TO_USE_THIS_BERKELEY_DB_VERSION 1
-setx BERKELEYDB_DIR %LIBRARY_PREFIX%
+setx BERKELEYDB_DIR %LIBRARY_INC%
 
 %PYTHON% -m pip install . --no-deps --ignore-installed

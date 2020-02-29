b95ed1550b5f9ba5e97dd4deda073618e73ac91d
shadow_walker <shadowwalkersb@gmail.com>
2020-02-28 22:42:53 -0600

    fix win script [skip travis]

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,4 @@
 set YES_I_HAVE_THE_RIGHT_TO_USE_THIS_BERKELEY_DB_VERSION=1
-BERKELEYDB_DIR=$PREFIX $PYTHON -m pip install . --no-deps --ignore-installed
+set BERKELEYDB_DIR=%PREFIX%
+
+%PYTHON% -m pip install . --no-deps --ignore-installed

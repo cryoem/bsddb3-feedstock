874ff07ce74db9cb9c9e5833b6399c5bd83410a0
shadow_walker <shadowwalkersb@gmail.com>
2020-02-28 23:20:15 -0600

    patch for win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
 setx YES_I_HAVE_THE_RIGHT_TO_USE_THIS_BERKELEY_DB_VERSION 1
-setx BERKELEYDB_DIR %LIBRARY_INC%
+setx BERKELEYDB_DIR %LIBRARY_PREFIX%
 
 %PYTHON% -m pip install . --no-deps --ignore-installed
